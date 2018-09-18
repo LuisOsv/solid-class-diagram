21ccfb8771e4935804372a549f8baa78ed371f39
Osmar <Osmar@Osmar-PC>
2018-09-17 23:56:49 -0400

    Test

---
+++ IdentificacionDeConceptos/src/concepts/CodigoInnecesario.java
@@ -0,0 +1,12 @@
+package concepts;
+
+public class CodigoInnecesario {
+	
+	public static void main(String args[]){
+		//Variables that are declared to print "hola mundo" although they are not necessary
+		String hola = "hola";
+		String mundo = "mundo";
+		//Print "Hola Mundo"
+		System.out.println(hola+" "+mundo);
+	}
+}
--- IdentificacionDeConceptos/src/concepts/CodigoSencillo.java
+++ IdentificacionDeConceptos/src/concepts/CodigoSencillo.java
@@ -1,5 +1,10 @@
-package test;
+package concepts;
 
-public class Test {
-int i =0;
+public class CodigoSencillo {
+
+	//Easy code that any person can understand
+	public static void main(String args[]){
+		//Print "Hola Mundo"
+		System.out.println("hola mundo");
+	}
 }
