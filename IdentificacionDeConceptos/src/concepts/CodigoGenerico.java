524e5184513dd321263873bffbf4247ccb363eb4
Osmar <Osmar@Osmar-PC>
2018-09-18 00:15:14 -0400

    Test

--- IdentificacionDeConceptos/src/concepts/CodigoGenerico.java
+++ IdentificacionDeConceptos/src/concepts/CodigoGenerico.java
@@ -1,12 +1,45 @@
 package concepts;
 
+/*
+ * Generic code write for a simple function that should print "hola mundo".
+ * A lot of code that is not needed was written.
+ */
 public class CodigoGenerico {
+	public String saludo;
+	public String texto;
 	public CodigoGenerico(String saludo, String texto){
-		
+		this.saludo = saludo;
+		this.texto = texto;
+	}
+	
+
+	public String getSaludo() {
+		return saludo;
 	}
 
+
+	public void setSaludo(String saludo) {
+		this.saludo = saludo;
+	}
+
+
+	public String getTexto() {
+		return texto;
+	}
+
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+	
+	public void printSaludo(){
+		System.out.println(saludo + " " + texto);
+	}
+
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		CodigoGenerico x = new CodigoGenerico("hola", "mundo");
+		x.printSaludo();
 
 	}
 
---
+++ IdentificacionDeConceptos/src/concepts/ProgramaAyudaDecirHolaMundo.java
@@ -0,0 +1,10 @@
+package concepts;
+
+public class ProgramaAyudaDecirHolaMundo {
+	//Program that helps to the user to say "hola mundo"
+	//The program accomplishes with the goal to help people that need to say "hola mundo"
+	public static void main(String args[]){
+		//Print "Hola Mundo"
+		System.out.println("hola mundo");
+	}
+}
